fe60a905eb2d5b1fad63655e412b54f733d171d9
kevin <9047112+kevL974@users.noreply.github.com>
2023-08-25 11:03:30 +0200

    adding get_historical_close_price and get_historical_close_time functions in utils.py. updating tests_utils.py.

--- bot_binance/utils.py
+++ bot_binance/utils.py
@@ -1,4 +1,6 @@
 import configparser
+from typing import List
+from bot_binance.binance_constants import HistKlinesIndex
 
 CONFIG_YAML_API_DEMO_SECTION = "api_binance_demo"
 CONFIG_YAML_API_SECTION = "api_binance_demo"
@@ -6,6 +8,45 @@ CONFIG_YAML_API_SECTION = "api_binance"
 CONFIG_YAML_API_KEY_VAR = "api_key"
 CONFIG_YAML_API_SECRET_VAR = "api_secret"
 
+
+def get_historical_close_price(hist_klines: List[List]) -> List[float]:
+    """
+    Returns list of close price values from historical candlelines.
+    @param hist_klines: Historical Klines from Binance
+    @return: list of close price values
+    """
+    assert hist_klines is not None
+    return list(map(float, [kline[HistKlinesIndex.CLOSE] for kline in hist_klines]))
+
+
+def get_historical_close_time(hist_klines: List[List]) -> List[float]:
+    """
+    Returns list of close price values from historical candlelines.
+    @param hist_klines: Historical Klines from Binance
+    @return: list of close price values
+    """
+    assert hist_klines is not None
+    return list(map(float, [kline[HistKlinesIndex.CLOSE_TIME] for kline in hist_klines]))
+
+
+def convert_timeunit_abrv_to_full(time_unit: str) -> str:
+    """
+    Function that converts time unit abbreviation to full name.
+    @param time_unit: time unit abbreviation to convert
+    @return: time unit in full name
+    """
+    if time_unit == "m":
+        return "minutes"
+    elif time_unit == "h":
+        return "hours"
+    elif time_unit == "d":
+        return "days"
+    elif time_unit == "w":
+        return "weeks"
+    elif time_unit == "M":
+        return "months"
+    else:
+        ValueError("'{}' abbreviation unknowned.".format(time_unit))
 
 def load_api_keys_from_yaml_file(path: str) -> configparser.ConfigParser:
     """
--- tests/tests_utils.py
+++ tests/tests_utils.py
@@ -44,6 +44,32 @@ class TestsUtils(unittest.TestCase):
                          "Wrong api key value : {expected} instead of {result}".format(expected=expected_api_secret_val,
                                                                                        result=api_secret_val))
 
+    def test_get_historical_close_price(self):
+        expected_hist_cp = [26046.0, 26057.74]
+        hist_klines = [[1692941220000, '26048.02000000', '26048.02000000', '26040.76000000', str(expected_hist_cp[0]),
+                        '2.66325800', 1692941279999, '69362.45675895', 74, '0.57613000', '15004.54066359', '0'],
+                       [1692941280000, '26046.00000000', '26058.57000000', '26045.99000000', str(expected_hist_cp[1]),
+                        '3.69770200', 1692941339999, '96331.26698120', 90, '3.69086400', '96153.11545508', '0']]
+        result_hist_cp = get_historical_close_price(hist_klines)
+        self.assertListEqual(expected_hist_cp,
+                             result_hist_cp,
+                             "get_historical_close_price doesn't return close prices values :"
+                             " {result} instead of {expected}".format(result=result_hist_cp, expected=expected_hist_cp))
+
+    def test_get_historical_close_time(self):
+        expected_hist_ct = [1692941279999, 1692941339999]
+        hist_klines = [
+            [1692941220000, '26048.02000000', '26048.02000000', '26040.76000000', '26046.00000000',
+             '2.66325800', expected_hist_ct[0], '69362.45675895', 74, '0.57613000', '15004.54066359', '0'],
+            [1692941280000, '26046.00000000', '26058.57000000', '26045.99000000', '26057.74000000',
+             '3.69770200', expected_hist_ct[1], '96331.26698120', 90, '3.69086400', '96153.11545508', '0']]
+        result_hist_ct = get_historical_close_time(hist_klines)
+        self.assertListEqual(expected_hist_ct,
+                             result_hist_ct,
+                             "get_historical_close_time doesn't return close time values :"
+                             " {result} instead of {expected}".format(result=result_hist_ct,
+                                                                      expected=expected_hist_ct))
+
 
 if __name__ == '__main__':
     unittest.main()
